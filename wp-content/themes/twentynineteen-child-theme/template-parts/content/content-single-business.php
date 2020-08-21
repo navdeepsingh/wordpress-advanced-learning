e0e9b58381b4a681d587605a0937ba1ffa7ebc94
Navdeep Singh <navdeep.er@gmail.com>
2020-08-21 14:22:39 +0530

    Update: Custom Header in child theme

--- wp-content/themes/twentynineteen-child-theme/template-parts/content/content-single-business.php
+++ wp-content/themes/twentynineteen-child-theme/template-parts/content/content-single-business.php
@@ -14,7 +14,7 @@
 <article id="post-<?php the_ID();?>" <?php post_class();?>>
 	<?php if (!twentynineteen_can_show_post_thumbnail()): ?>
 	<header class="entry-header">
-		<?php get_template_part('template-parts/header/entry', 'header');?>
+		<?php get_template_part('template-parts/header/entry-header', 'business');?>
 	</header>
 	<?php endif;?>
 
